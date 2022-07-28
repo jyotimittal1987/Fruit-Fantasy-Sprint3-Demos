6f1ba8370e7054e58774beaea278e3ae0d754ad1
jyotimittal1987 <jyotimittal1987@gmail.com>
2022-07-28 13:50:42 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FruitManagerComponent } from './fruit-manager/fruit-manager.component';
+import { FruitListComponent } from './fruit-list/fruit-list.component';
+import { FruitCardComponent } from './fruit-card/fruit-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FruitManagerComponent,
+    FruitListComponent,
+    FruitCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
